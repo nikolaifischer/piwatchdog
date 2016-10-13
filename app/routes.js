1edcefac774dc8a557b14eb2dc71eef66fa203f3
nikolaifischer <nfischer@hm.edu>
2016-10-13 11:31:41 +0200

    Backend Refactoring
    - Added watchengine.js f
    - Added Link to index in logo

--- app/routes.js
+++ app/routes.js
@@ -4,16 +4,12 @@ var website = require('./models/website');
 var settings = require('./models/settings');
 var http = require('http');
 var URL = require('url-parse');
-var Repeat = require('repeat');
 var fs = require('fs');
 
 var mailer = require('./mailer');
 var screenshot = require ('./screenshot');
 var pushbullet = require('./pushbullet');
-
-
-var watching = [];
-
+var watchengine = require ('./watchengine');
 
 
 mongoose.connect('mongodb://localhost/piwatchdog');
@@ -25,7 +21,7 @@ var alreadyNotified = [];
 
 module.exports = function(app) {
 
-    startWatchingAgain();
+    watchengine.startWatchingAgain();
 
     // server routes ===========================================================
     // handle things like api calls
@@ -69,7 +65,7 @@ module.exports = function(app) {
 
             res.json({ message: 'Site has been added!', data: newSite });
 
-            registerWatcher(newSite.id,newSite.interval);
+            watchengine.registerWatcher(newSite.id,newSite.interval);
             screenshot.takeScreenshot(newSite.url, newSite.id);
 
           });
@@ -79,7 +75,7 @@ module.exports = function(app) {
 
     app.delete('/api/websites/:id', function(req, res) {
         console.log("Deleting in DB the website with the id " +req.params.id);
-        deregisterWatcher(req.params.id);
+        watchengine.deregisterWatcher(req.params.id);
         website.findByIdAndRemove(req.params.id, function(err) {
             if (err)
               res.send(err);
@@ -96,7 +92,7 @@ module.exports = function(app) {
         if (err)
           res.send(err);
 
-        deregisterWatcher(website.id);
+        watchengine.deregisterWatcher(website.id);
         website.name = req.body.name;
         website.interval = req.body.interval;
         website.url = req.body.url;
@@ -104,7 +100,7 @@ module.exports = function(app) {
 
         
         website.save(function(err,website){
-             registerWatcher(website.id, website.interval);
+             watchengine.registerWatcher(website.id, website.interval);
              screenshot.takeScreenshot(website.url, website.id);
 
         });
@@ -140,15 +136,12 @@ module.exports = function(app) {
         
         // use mongoose to get all websites in the database
         settings.find({}).maxTime(1000).exec(function(err, settings) {
-            
 
-            // if there is an error retrieving, send the error. 
-            // nothing after res.send(err) will execute
 
             if (err)
             {
                 res.send(err);
-                console.log(err);
+                return;
             }
             res.json(settings); // return all websites in JSON format
         });
@@ -196,163 +189,6 @@ module.exports = function(app) {
 
 
 
-    function checkOnlineStatus( id) {
-        
-        // Get url from DB
-
-        console.log("checking Online Status of "+ id);
-
-        website.findById(id, function(err, website) 
-        {
-
-            if (err) {
-                console.log("Probably DB Error!")
-                console.log(err);
-            }
-
-               var url =  new URL(website.url);
-
-               var options = {
-                  host: url.hostname,
-                  port: url.port,
-                  path: url.pathname
-                };
-
-
-                http.get(options, function(res) {
-                  console.log("Got response: " + res.statusCode);
-                  if(res.statusCode == 200 || res.statusCode == 302) {
-                    website.isOnline = true;
-                    website.last_checked = Date.now();
-                    website.save();
-                  }
-                }).on('error', function(e) {
-                    // Hier müsste noch gecheckt werden, ob nicht einfach das WLAN aus ist.
-                  console.log("Got error: " + e.message);
-                  website.isOnline = false;
-                  website.last_checked = Date.now();
-                  website.save();
-
-                  // Send Pushbullet?
-
-                  settings.find({}, function(err, settingsArray){
-
-                    var settings = settingsArray[settingsArray.length-1];
-
-                    console.log(settings);
-
-                    if (err) {
-                        console.log("DB Error:")
-                        console.log(err);
-                        return;
-                    }
-
-                    if(alreadyNotified.indexOf(id)<0) {
-                        if(settings.sendPushbullet) {
-                            pushbullet.sendPush( website.name+" is offline", website.url +" just went offline. I last checked at "+website.last_checked,settings.pushbulletAPI);
-                        }
-
-                        if(settings.sendEmail) {
-                            mailer.sendEmail("Website "+website.name+" is offline", "The website with the url "+website.url+" appears to be offline!", settings);
-                        }
-                        alreadyNotified.push(id);
-                    }
-
-                  });
-
-                });
-
-        });
-
-       
-
-    }
-
-
-
-    function registerWatcher(id, interval) {
-
-        console.log("Register Watcher for "+ id);
-
-        // if ID not in "watching" array
-            // call repeat function with checkonlineStatus for new id
-            // add ID to "watching" array
-
-        if(inWatching(id)==false){    
-            Repeat(function(){return checkOnlineStatus(id); }).every(interval, 'minutes').until(function(){return inWatching(id)==false}).start.now();
-            watching.push(id);
-        }
-
-    }
-
-
-     // removes site from watching Array
-    function deregisterWatcher (id) {
-
-        var index ;
-
-        for (var i = 0; i<watching.length; i++) {
-            if (id == watching[i]) {
-                index = i;
-            }
-        }
-    
-        // Delete the id from the watching array.
-        watching.splice(index,1);
-
-        var filePath = "./public/media/screenshots/"+id+".png" ; 
-        // Check if there is a file and then delete it
-        fs.stat(filePath,function(err, stat){
-            if(err) {
-                return;
-            }
-            else if(stat.isFile()){
-                fs.unlink(filePath);
-            }
-        });
-       
-       
-    }
-
-
-
-    // This function is called, when the server is started. All websites from the DB are registered again at the watcher.
-    function startWatchingAgain() {
-
-      website.find({}).maxTime(2000).exec(function(err, websites) {
-
-        if(err) {
-            console.log (err);
-            return;
-        }
-
-        for (var i=0 ; i<websites.length; i++) {
-            registerWatcher(websites[i]._id, websites[i].interval);
-
-            // Make new Screenshots:
-
-            screenshot.takeScreenshot(websites[i].url, websites[i]._id);
-        }
-      });
-
-      return;
-
-    }
-
-
-
-
-    
-     // returns true if website is in watching array
-    function inWatching (id) {
-       
-        for (var i = 0; i<watching.length; i++) {
-            if (id == watching[i]) {
-                return true;
-            }
-        }
-        return false;
-    }
 
 
 
---
+++ app/watchengine.js
@@ -0,0 +1,168 @@
+var Repeat = require('repeat');
+var website = require('./models/website');
+var settings = require ('./models/settings');
+var screenshot = require('./screenshot.js');
+var http = require('http');
+var URL = require('url-parse');
+
+var watching = [];
+
+exports.registerWatcher = function (id, interval) {
+
+    console.log("Register Watcher for "+ id);
+
+    // if ID not in "watching" array
+        // call repeat function with checkonlineStatus for new id
+        // add ID to "watching" array
+
+    if(exports.inWatching(id)==false){    
+        Repeat(function(){return checkOnlineStatus(id); }).every(interval, 'minutes').until(function(){return exports.inWatching(id)==false}).start.now();
+        watching.push(id);
+    }
+
+}
+
+
+ // removes site from watching Array
+exports.deregisterWatcher =function (id) {
+
+    var index ;
+
+    for (var i = 0; i<watching.length; i++) {
+        if (id == watching[i]) {
+            index = i;
+        }
+    }
+
+    // Delete the id from the watching array.
+    watching.splice(index,1);
+
+    var filePath = "./public/media/screenshots/"+id+".png" ; 
+    // Check if there is a file and then delete it
+    fs.stat(filePath,function(err, stat){
+        if(err) {
+            return;
+        }
+        else if(stat.isFile()){
+            fs.unlink(filePath);
+        }
+    });
+   
+   
+}
+
+
+
+// This function is called, when the server is started. All websites from the DB are registered again at the watcher.
+exports.startWatchingAgain = function() {
+
+  website.find({}).maxTime(2000).exec(function(err, websites) {
+
+    if(err) {
+        console.log (err);
+        return;
+    }
+
+    for (var i=0 ; i<websites.length; i++) {
+        exports.registerWatcher(websites[i]._id, websites[i].interval);
+
+        // Make new Screenshots:
+
+        screenshot.takeScreenshot(websites[i].url, websites[i]._id);
+    }
+  });
+
+  return;
+
+}
+
+
+ // returns true if website is in watching array
+exports.inWatching = function (id) {
+   
+    for (var i = 0; i<watching.length; i++) {
+        if (id == watching[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+
+
+function checkOnlineStatus( id) {
+    
+    // Get url from DB
+
+    console.log("checking Online Status of "+ id);
+
+    website.findById(id, function(err, website) 
+    {
+
+        if (err) {
+            console.log("Probably DB Error!")
+            console.log(err);
+        }
+
+           var url =  new URL(website.url);
+
+           var options = {
+              host: url.hostname,
+              port: url.port,
+              path: url.pathname
+            };
+
+
+            http.get(options, function(res) {
+              console.log("Got response: " + res.statusCode);
+              if(res.statusCode == 200 || res.statusCode == 302) {
+                website.isOnline = true;
+                website.last_checked = Date.now();
+                website.save();
+              }
+            }).on('error', function(e) {
+                // Hier müsste noch gecheckt werden, ob nicht einfach das WLAN aus ist.
+              console.log("Got error: " + e.message);
+              website.isOnline = false;
+              website.last_checked = Date.now();
+              website.save();
+
+              // Send Pushbullet?
+
+              settings.find({}, function(err, settingsArray){
+
+                var settings = settingsArray[settingsArray.length-1];
+
+                console.log(settings);
+
+                if (err) {
+                    console.log("DB Error:")
+                    console.log(err);
+                    return;
+                }
+
+                if(alreadyNotified.indexOf(id)<0) {
+                    if(settings.sendPushbullet) {
+                        pushbullet.sendPush( website.name+" is offline", website.url +" just went offline. I last checked at "+website.last_checked,settings.pushbulletAPI);
+                    }
+
+                    if(settings.sendEmail) {
+                        mailer.sendEmail("Website "+website.name+" is offline", "The website with the url "+website.url+" appears to be offline!", settings);
+                    }
+                    alreadyNotified.push(id);
+                }
+
+              });
+
+            });
+
+    });
+
+   
+
+}
+
+
+
